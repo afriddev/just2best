27b3291444817b823ab9416bcf642514bcf66cb8
AFRID SHAIK <afridayan01@gmail.com>
2023-12-29 11:26:43 +0000

    commit

--- app/api/getUser/route.ts
+++ app/api/getUser/route.ts
@@ -2,6 +2,12 @@ import connectMongoDb from "@/libs/dataBase/mongoDb"
 import userModel from "@/libs/models/user"
 import { NextResponse } from "next/server"
 
+const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  };
+
 export async function POST(req){
 
     let {email,password} = await req.json()
@@ -10,12 +16,12 @@ export async function POST(req){
        await connectMongoDb()
        let result = await userModel.find({emailId:email,password:password})
        if(result.length > 0){
-        return NextResponse.json(result[0]);
+        return NextResponse.json(result[0],{ headers: corsHeaders });
        }
        else{
         return NextResponse.json({
             message:"notFount"
-        })
+        },{ headers: corsHeaders })
        }
     }
     catch(e){
